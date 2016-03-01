23b272848ffabc53a4f9b32862dce62613e48638
Jesla <jesslar415@gmail.com>
2016-03-01 09:07:11 -0600

    Push - first letter highlight

--- script.js
+++ script.js
@@ -17,51 +17,15 @@ $(document).ready(function() {
         }
     })
     
-
-    
-        $(this).keydown(function(e){
-        if (e.which === 74) {
-            $("e").id = j
-
+    $(document).keydown(function(e) {
+        $("#" + e.which) 
+        $("#113").css({ "background": "blue",
+               "color": "white"});
     });
-    
-    
-    
-    
-        // console.log(e.which); 
-    
-    //     $(this).keyup(function(e){
-    //     if (e.which === 16) {
-    //         $("#keyboard-upper-container").hide();
-    //         $("#keyboard-lower-container").show();  
-    //     }
-    // })
-    
-    //e.which
-   //change ids to numbers
-  //on click, change css for button background color to blue and letter on button to white
-  
-  //when we press a key, add a class to the dom - highlight
-  //corresponding ascii with id for key
-  
-  //change ascii to symbol
-  
-  
-    // $(".well well-lg key").keypress(function(e) {
-    // $(this).append($(this).css({
-    // "background-color": "blue",
-    // "color": "white"
-    // }));
-    
-    
-// e.which
-// make a class "highlight" that has the css for what would happen when you press a key,
-// keyup
-    
+        $(document).keyup(function(e) {
+        $("#" + e.which) 
+        $("#113").css({ "background": "",
+               "color": ""});
+    });
 
-//     event listener for a keypress -->function(e) {
-//         console.log(e.which);
-//         charCode(54) will become A or whatever
-//     }
-    
-// turn ascii into text
+// to move yellow box - css calculation to change position as letters are typed.
